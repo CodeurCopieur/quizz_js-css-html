9881d237f07c968deb1e3de600a132cef05bb5aa
codwerk <louis_widdy@live.fr>
2020-12-12 17:48:28 +0100

    obtenir les resultats

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const form = document.querySelector('.form-quizz');
-let tabResults = [];
+let tabResultats = [];
+const reponses = ['b', 'a', 'a', 'a', 'b']
+const emojis = ['🤩', '✨', '🙄', '😭', '👎🏾'];
+const titreResultat = document.querySelector('.resultats h2');
+const textResultat = document.querySelector('.resultats .note');
+const aideResultat = document.querySelector('.resultats .aide');
+const lesquestions = document.querySelectorAll('.question-block');
+let verifTableau = []; //tableau de true ou false
+
 
 form.addEventListener('submit', (e)=> {
   e.preventDefault();
@@ -7,8 +15,24 @@ form.addEventListener('submit', (e)=> {
   console.log(document.querySelector('input[name="q1"]:checked').value);
 
   for (let index = 1; index < 6; index++) {
-    tabResults.push(document.querySelector(`input[name="q${index}"]:checked`).value)
-    console.log(tabResults);
+    tabResultats.push(document.querySelector(`input[name="q${index}"]:checked`).value)
   }
+
+  verifFunc(tabResultats);
+  tabResultats = []
 })
-console.log(form);
+
+function verifFunc(tabResults) {
+
+  for (let index = 0; index < 5; index++) {
+
+    if(tabResults[index] === reponses[index]) {
+      verifTableau.push(true);
+    } else {
+      verifTableau.push(false);
+    }
+
+    console.log(verifTableau);
+    verifTableau = [];
+  }
+}
